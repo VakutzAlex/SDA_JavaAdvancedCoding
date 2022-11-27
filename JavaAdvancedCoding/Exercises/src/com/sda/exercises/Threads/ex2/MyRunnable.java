175aa3b8671cd6cf61180cade07f767cf7eb4222
Vakutz Alex <vacut_alex@yahoo.com>
2022-11-27 12:42:20 +0200

    runnable

---
+++ JavaAdvancedCoding/Exercises/src/com/sda/exercises/Threads/ex2/MyRunnable.java
@@ -0,0 +1,17 @@
+package com.sda.exercises.Threads.ex2;
+
+/**
+ * Create a class implementing Runnable, e.g. MyRunnable.
+ * Implement the run() method, which will display the name of the current thread in the same way as in the previous exercise.
+ * Create a class with the public static void main (String [] args) method.
+ * Inside the main method, create a class type variable created above, e.g. MyRunnable, and initialize the class.
+ * Create a Thread variable and initialize it by passing the Runnable interface implementation as the constructor parameter.
+ * Perform the start() method on a Thread type variable.
+ */
+
+public class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+    }
+}
